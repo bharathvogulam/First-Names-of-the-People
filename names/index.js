1d1709f305da5ea464e3d0e04a7c5e23d1b080cc
bharathreddy <bharathreddyvogulam99@gmail.com>
2022-10-18 12:59:00 +0000

    commitnewname

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const res = require("../country/state/city/index");
+const z = require("../utilities/utils/index");
+
+const getPeopleInCity = (res) => {
+  return z(res);
+};
+
+module.exports = getPeopleInCity;
